96fe2502a107a05656e864db4e561fa78c76601f
diegodls <dark.dls.lol@gmail.com>
2019-10-16 16:46:15 -0300

    Update

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://192.168.100.17:3333'
+    baseURL: 'SUA_URL_AQUI'
+    //exemplo de URL = 'http://192.168.1.1:3333', essa url é a url do seu servidor/pc seguida pela porta de comunicação
 });
 
 export default api;
